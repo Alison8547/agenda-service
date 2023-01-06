05eb9252ed8a95113786060d51f06f06512e8e79
Alison8547 <alisonailson100@hotmail.com>
2023-01-05 22:48:05 -0300

    feat: documentacao agenda service

--- src/main/java/com/api/agenda/controller/PacienteController.java
+++ src/main/java/com/api/agenda/controller/PacienteController.java
@@ -5,6 +5,9 @@ import com.api.agenda.model.Paciente;
 import com.api.agenda.request.PacienteRequest;
 import com.api.agenda.response.PacienteResponse;
 import com.api.agenda.service.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +27,14 @@ public class PacienteController {
     private final PacienteService service;
     private final PacienteMapper mapper;
 
+    @Operation(summary = "Listar todos os pacientes", description = "Listar pacientes do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Listou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<PacienteResponse>> list() {
 
@@ -31,6 +42,14 @@ public class PacienteController {
         return ResponseEntity.status(HttpStatus.OK).body(pacienteResponses);
     }
 
+    @Operation(summary = "Salvar um paciente", description = "Salvar um paciente no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Salvou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping
     public ResponseEntity<PacienteResponse> salvar(@Valid @RequestBody PacienteRequest paciente) {
 
@@ -39,6 +58,14 @@ public class PacienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toPacienteResponse(pacienteSalvo));
     }
 
+    @Operation(summary = "Pegar um paciente por id", description = "Pegar um paciente no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Resgatou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @GetMapping("/{idPaciente}")
     public ResponseEntity<PacienteResponse> buscarByIdPaciente(@PathVariable(name = "idPaciente") Long id) {
 
@@ -46,6 +73,14 @@ public class PacienteController {
         return ResponseEntity.status(HttpStatus.OK).body(mapper.toPacienteResponse(paciente.get()));
     }
 
+    @Operation(summary = "Atualizar um paciente", description = "Atualizar um paciente no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Atualizou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PutMapping("/{idPaciente}")
     public ResponseEntity<PacienteResponse> alterarPaciente(@PathVariable(name = "idPaciente") Long id, @Valid @RequestBody PacienteRequest paciente) {
 
@@ -54,6 +89,14 @@ public class PacienteController {
         return ResponseEntity.status(HttpStatus.OK).body(mapper.toPacienteResponse(pacienteSalvo));
     }
 
+    @Operation(summary = "Apagar um paciente", description = "Apagar um paciente um paciente no banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "204", description = "Apagou com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @DeleteMapping("/{idPaciente}")
     public ResponseEntity<Void> deletePaciente(@PathVariable(name = "idPaciente") Long id) {
         service.deletar(id);
--- src/main/java/com/api/agenda/request/PacienteRequest.java
+++ src/main/java/com/api/agenda/request/PacienteRequest.java
@@ -1,5 +1,6 @@
 package com.api.agenda.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -7,14 +8,17 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class PacienteRequest {
     @NotBlank(message = "Nome do paciente é obrigatorio!")
+    @Schema(description = "Nome do paciente",defaultValue = "José")
     private String nome;
 
     @NotBlank(message = "Sobrenome do paciente é obrigatorio!")
+    @Schema(description = "Sobrenome do paciente",defaultValue = "Silva")
     private String sobrenome;
 
     @NotBlank(message = "Cpf do paciente é obrigatorio!")
+    @Schema(description = "Cpf do paciente",defaultValue = "111-222-333-44")
     private String cpf;
-
+    @Schema(description = "Email do paciente",defaultValue = "jose@hotmail.com")
     private String email;
 
 }
